6494fd20443dd6322d7b4a82c9900bd8b08fa5f2
Vasanth Kesavan <vk432@njit.edu>
2018-05-24 01:38:44 -0400

    basic frontend template of successful socket connection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as io from 'socket.io-client';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  socket: any;
+
+  constructor() {
+    this.socket = io('http://localhost:3000/');
+    this.socket.on('connect', () => console.log('Frontend connection open'));
+    this.socket.on('sample_emit_value', function(val) {
+      console.log(val);
+    })
+  }
+
+  columnDefs = [
+    {headerName: 'Make', field: 'make', editable: true },
+    {headerName: 'Model', field: 'model' },
+    {headerName: 'Price', field: 'price'}
+  ];
+
+  rowData = [
+      { make: 'Toyota', model: 'Celica', price: 35000 },
+      { make: 'Ford', model: 'Mondeo', price: 32000 },
+      { make: 'Porsche', model: 'Boxter', price: 72000 }
+  ];
+
+  emitSocket() {
+    this.socket.emit('sample_emit_value', 'HELLO SERVER!');
+  }
 }
