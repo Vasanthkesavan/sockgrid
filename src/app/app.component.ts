ac311968bab914aea5dcb6a6e765ddbfea8863bd
Vasanth Kesavan <vk432@njit.edu>
2018-05-26 07:53:04 -0400

    saving default layout on grid ready and switches back to original layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as io from 'socket.io-client';
 import { SocketService } from './socket.service';
+import {GridOptions} from "ag-grid/main";
 
 @Component({
   selector: 'app-root',
@@ -12,8 +13,25 @@ export class AppComponent implements OnInit {
   socket: any;
   private gridApi;
   private gridColumnApi;
+  public columnDefs;
+  public rowData;
+  private layout;
+  private defaultLayout;
+  public gridOptions: GridOptions;
 
   constructor(private socketService: SocketService) { 
+    this.columnDefs = [
+      {headerName: 'Make', field: 'make', editable: true },
+      {headerName: 'Model', field: 'model' },
+      {headerName: 'Price', field: 'price'}
+    ];
+  
+    this.rowData = [
+        { make: 'Toyota', model: 'Celica', price: 35000 },
+        { make: 'Ford', model: 'Mondeo', price: 32000 },
+        { make: 'Porsche', model: 'Boxter', price: 72000 }
+    ];
+    this.gridOptions = <GridOptions>{};
   }
   
   ngOnInit() {
@@ -24,18 +42,6 @@ export class AppComponent implements OnInit {
     });
   }
 
-  columnDefs = [
-    {headerName: 'Make', field: 'make', editable: true },
-    {headerName: 'Model', field: 'model' },
-    {headerName: 'Price', field: 'price'}
-  ];
-
-  rowData = [
-      { make: 'Toyota', model: 'Celica', price: 35000 },
-      { make: 'Ford', model: 'Mondeo', price: 32000 },
-      { make: 'Porsche', model: 'Boxter', price: 72000 }
-  ];
-
   onCellValueChanged(params) {
     let rowId = params.node.id;
     let colId = params.column.colId;
@@ -44,13 +50,29 @@ export class AppComponent implements OnInit {
     this.socketService.updateAColumn({ rowId: rowId, colId: colId, newValue: newValue, oldValue: oldValue });
   }
 
+  changeColumnState(){
+    this.gridColumnApi.setColumnState(this.defaultLayout)
+    // console.log(this.defaultLayout)
+  }
+
   onGridReady(params) {
     this.gridApi = params.api;
     this.gridColumnApi = params.columnApi;
+    this.defaultLayout = this.gridColumnApi.getColumnState();
+
     this.socketService.getUpdatedColumn()
       .subscribe(data => {
         let rowNode = this.gridApi.getRowNode(data.rowId);
         rowNode.setDataValue(data.colId, data.newValue);
+      });
+
+      this.gridApi.addGlobalListener(function(type, event) {
+        if(type === 'dragStopped') {
+          params.columnApi.setColumnState(params.columnApi.getColumnState());
+          console.log(params.columnApi.getColumnState())
+        }
       })
+
   }
+
 }
